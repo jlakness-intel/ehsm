048e8ff8021e4a3d8ea95c216db51259fd50ff27
John Lakness <84150418+jlakness-intel@users.noreply.github.com>
2022-11-07 14:30:42 -0800

    all services on one container except couchdb

--- docker/all/start_all.sh
+++ docker/all/start_all.sh
@@ -1,10 +1,5 @@
-#!/bin sh
-echo '# PCCS server address' > /etc/sgx_default_qcnl.conf
-echo 'PCCS_URL='${PCCS_URL}'/sgx/certification/v3/' >> /etc/sgx_default_qcnl.conf
-echo '# To accept insecure HTTPS certificate, set this option to FALSE' >> /etc/sgx_default_qcnl.conf
-echo 'USE_SECURE_CERT=FALSE' >> /etc/sgx_default_qcnl.conf
-
-node /home/ehsm/ehsm_kms_service/ehsm_kms_server.js &
-sh -c /home/ehsm/out/ehsm-dkeyserver/ehsm-dkeyserver &
-sh -c "/home/ehsm/out/ehsm-dkeycache/ehsm-dkeycache -i ${DKEYSERVER_IP} -p ${DKEYSERVER_PORT}" &
-
+#!/bin/sh
+cd /opt/intel/pccs && node -r esm /opt/intel/pccs/pccs_server.js &
+cd /home/ehsm/out/ehsm-dkeyserver && /home/ehsm/out/ehsm-dkeyserver/ehsm-dkeyserver &
+cd /home/ehsm/out/ehsm-dkeycache && ./ehsm-dkeycache -i 127.0.0.1 -p 8888 &
+cd /home/ehsm/ehsm_kms_service/ && node /home/ehsm/ehsm_kms_service/ehsm_kms_server.js 
