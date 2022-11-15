cd0cf7345cc2b74d425a8a3974795c11dbf5b2d9
John Lakness <84150418+jlakness-intel@users.noreply.github.com>
2022-11-15 12:10:03 -0800

    simpler volumes

--- docker/pccs/run_pccs.sh
+++ docker/pccs/run_pccs.sh
@@ -3,9 +3,8 @@ docker run \
       --name pccs \
       -it \
       --cpu-shares 512 --pids-limit 100 --memory=512m --security-opt=no-new-privileges \
-      -v `pwd`/certs/private.pem:/opt/intel/pccs/ssl_key/private.pem \
-      -v `pwd`/certs/file.crt:/opt/intel/pccs/ssl_key/file.crt \
-      -v `pwd`/config/pccs.config:/opt/intel/pccs/config/default.json \
+      -v `pwd`/certs:/opt/intel/pccs/ssl_key \
+      -v `pwd`/config:/opt/intel/pccs/config \
       --read-only --tmpfs /opt/intel/pccs/logs/ --tmpfs /tmp \
       --health-cmd='pgrep -f  "/usr/bin/node -r esm pccs_server.js" || exit 1' \
       intel/pccs
